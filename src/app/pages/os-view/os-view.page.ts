b3f44996dd8eef8f07d0242b89b61a7cd40ecad8
fertunouti <ftunouti@edu.unifil.br>
2023-08-15 08:31:40 -0300

    view-os

--- src/app/pages/os-view/os-view.page.ts
+++ src/app/pages/os-view/os-view.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApiService } from 'src/app/services/apiService';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-os-view',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./os-view.page.scss'],
 })
 export class OsViewPage implements OnInit {
+  private pedidoCanceladoSubscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private apiService:ApiService, private eventService: EventService) { 
+    this.pedidoCanceladoSubscription = this.eventService.pedidoCancelado$.subscribe(() => {
+      this.getPedidosAndRefresh();
+    });
   }
+  tipoUser!: string
+  idPedido!: number
+  pedido!: any
+  ngOnInit() {
+    this.tipoUser = this.apiService.getUserRole()
+    this.idPedido = this.apiService.readId()
+    this.getPedidosAndRefresh();
+ 
+  }
+  ngOnDestroy(): void {
+    this.pedidoCanceladoSubscription.unsubscribe();
+  }
+
+  private getPedidosAndRefresh() {
+    this.apiService.getPedidos().subscribe(
+      (data) => {
+        this.pedido = data;
+        console.log('Pedidos no histórico de pedidos:', this.pedido);
+      },
+      (error) => {
+        console.error('Erro ao obter dados dos pedidos:', error);
+      }
+    );
+  }
+
+
 
 }
